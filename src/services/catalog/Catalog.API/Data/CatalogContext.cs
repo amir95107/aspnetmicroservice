981fd588d7038f39f2af371727c64239e1fea3e3
amir95107 <a.janmohammaid@gmail.com>
2022-01-17 00:47:26 +0330

    repo

---
+++ src/services/catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuraion)
+        {
+            var client = new MongoClient(configuraion.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuraion.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuraion.GetValue<string>("DatabaseSettings:CollectionName"));
+            //CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
